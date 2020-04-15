b7c556fc8d987e17b426066e5574f5115e5ad98a
ernestoha <ernestoha@gmail.com>
2020-04-15 17:58:38 -0400

    First Assignment.

---
+++ song.js
@@ -0,0 +1,22 @@
+/**
+   * Assignment 01 ES06.
+   * Favorite song and its attributes.
+   */
+
+//Define:
+var song = {
+   name : "Soy Cubano, Soy Popular",
+   artist : "La Charanga Habanera",
+   genre : "Salsa",
+   covers : ["classic", "country", "kpop"],
+   durationInSeconds : 396,
+   isFavorite: true
+};
+
+//Output:
+console.log(song.name);
+console.log(song.artist);
+console.log(song.genre);
+console.log(song.covers);
+console.log(song.durationInSeconds);
+console.log(song.isFavorite);
